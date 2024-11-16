9282e1d8e97e224d348889d625b9d11cd6c21c35
VoMinhLoi <minhloi1131130@gmail.com>
2024-11-16 20:54:05 +0700

    Clear and focus input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { useRef } from "react";
 import { useStore, actions } from "./store";
 function App() {
   const [state, dispatch] = useStore();
   const { toDos, toDo } = state;
+  const inputRef = useRef();
+  const hanldeSubmit = (e) => {
+    dispatch(actions.addToDoInput(toDo));
+    dispatch(actions.setToDoInput(""));
+    inputRef.current.focus();
+  };
   return (
     <div className="App">
       <h1>Mobifone's menu food</h1>
       <input
+        ref={inputRef}
         value={toDo}
         onChange={(e) => {
           dispatch(actions.setToDoInput(e.target.value));
         }}
       />
-      <button onClick={(e) => dispatch(actions.addToDoInput(toDo))}>add</button>
+      <button onClick={hanldeSubmit}>add</button>
       <ul>
         {toDos.map((item, index) => (
           <li key={index}>{item}</li>
